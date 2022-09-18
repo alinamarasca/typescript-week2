18efa70bc5097f8d71a83725dc4e20b88c32982e
Alina <alinamarasca@gmail.com>
2022-09-18 15:45:55 +0200

    add types

--- assessment2/main.ts
+++ assessment2/main.ts
@@ -1,6 +1,12 @@
-function sumOfMultiple(limit: any): any {
-    // Your code goes here
-    return 0;
+function sumOfMultiple(limit: number): number {
+  // Your code goes here
+  let arr: number[] = [];
+  for (let i: number = 0; i < limit; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      arr.push(i);
+    }
+  }
+  return arr.reduce((acc, curr) => acc + curr, 0);
 }
 
 module.exports = sumOfMultiple;
