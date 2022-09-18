cdb0ae570dbf2363bc374790e125878c68bbba45
Alina <alinamarasca@gmail.com>
2022-09-18 15:32:07 +0200

    assessment1

--- assessment1/main.ts
+++ assessment1/main.ts
@@ -1,98 +1,108 @@
 // Use type and mentioned in the name of the variable
 // Assign an appropriate value
-const aNumber: any = null;
-const aString: any = null;
-const aBigInt: any = null;
-const anArray: any = null
-const anUnknown: any = null;
-const anObject: any = null;
-const aTuple: any = null;
-const anEnum: any = null;
+
+const aNumber: number = 8;
+const aString: string = "string";
+const aBigInt: BigInt = 9007199254740991n;
+const anArray: Array<string> = ["ar", "ray"];
+const anUnknown: unknown = "Maybe string, maybe not, he-he-he";
+const anObject: Object = { name: "HYF", type: "TypeScript course" };
+const aTuple: [number, string] = [1, "TypeScript"];
+enum Direction {
+  Up = "Up",
+  Down = "Down",
+  Left = "Left",
+  Right = "Right"
+}
+const anEnum: Direction = Direction.Up;
+// usage eg: move(Direction.Down)
 
 // Implement this function any type accordingly
-function isEven(number): any {
-
+function isEven(number: number): boolean {
+  return number % 2 === 0;
 }
 
 // Type the following following function and variable
 type Person = {
-    name: string
-}
+  name: string;
+};
 
 type Developer = {
-    role: string
-}
+  role: string;
+};
 
-const jane: any = {
-    name: 'Jane'
-}
+const jane: Person = {
+  name: "Jane"
+};
 
-const joe: any = {
-    role: 'Devops'
-}
+const joe: Developer = {
+  role: "Devops"
+};
 
-const people: any[] =  [
-    jane, joe
-]
+const people: (Person | Developer)[] = [jane, joe];
 
-function printPeople(people: any[]): any {
-    // Print the name of person or the role in this function
+function printPeople(people: Person | Developer) {
+  // Print the name of person or the role in this function
+  let info: string;
+  if ("name" in people) {
+    info = people.name;
+  } else {
+    info = people.role;
+  }
+  console.log(`${info}`);
 }
 
 // Type the encrpyt function correctly
-const encryptText = (plainText: any, shift: any) => {
-    let cipherArr: any[] = []
-    let cipherLetter
+const encryptText = (plainText: string, shift: number) => {
+  let cipherArr: any[] = [];
+  let cipherLetter: number | string;
 
-    plainText.split("").map(letter => {
-      let code = letter.charCodeAt(letter)
-      if(letter === " ") {
-        return cipherArr.push(letter)
-      }
-      // Uppercase letters
-      if (code >= 65 && code <= 90) {
-        cipherLetter = String.fromCharCode(((code - 65 + shift) % 26) + 65)
-      }
-      // Lowercase letters
-      else if (code >= 97 && code <= 122) {
-        cipherLetter = String.fromCharCode(((code - 97 + shift) % 26) + 97)
-      }
-      return cipherArr.push(cipherLetter)
-    })  
-    return cipherArr.join("")
-}
-
+  plainText.split("").map(letter => {
+    let code = letter.charCodeAt(0);
+    if (letter === " ") {
+      return cipherArr.push(letter);
+    }
+    // Uppercase letters
+    if (code >= 65 && code <= 90) {
+      cipherLetter = String.fromCharCode(((code - 65 + shift) % 26) + 65);
+    }
+    // Lowercase letters
+    else if (code >= 97 && code <= 122) {
+      cipherLetter = String.fromCharCode(((code - 97 + shift) % 26) + 97);
+    }
+    return cipherArr.push(cipherLetter);
+  });
+  return cipherArr.join("");
+};
 
 // Solve using a literal union type
-let vegetable: any; // :Vegetable:
+let vegetable: "Cucumber" | "Eggplant" | "Cabbage"; // :Vegetable:
 vegetable = "Cucumber";
 vegetable = "Eggplant";
 vegetable = "Cabbage";
 
-
 // Solve the following exercise with an intersection type
 type Complainer = {
-    complains: () => void
-}
+  complains: () => void;
+};
 
 type Beerdrinker = {
-    drinkBeer: () => void
-}
+  drinkBeer: () => void;
+};
 
 type ChocolateLover = {
-    eatChocolate: () => void
-}
+  eatChocolate: () => void;
+};
 
 type Belgian = Complainer & Beerdrinker & ChocolateLover;
-function doBelgianThings(belgian: Belgian): void
-{
-    belgian.complains();
-    belgian.drinkBeer();
-    belgian.eatChocolate();
+function doBelgianThings(belgian: Belgian): void {
+  belgian.complains();
+  belgian.drinkBeer();
+  belgian.eatChocolate();
 }
 
 module.exports = {
-    isEven,
-    print,
-    encryptText
+  isEven,
+  print,
+  encryptText
 };
